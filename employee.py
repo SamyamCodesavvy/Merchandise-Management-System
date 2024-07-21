cc10fd6cdf8c03d37b6212892acd86fec901f15f
SamyamCodesavvy <samyam0913@gmail.com>
2024-07-21 20:28:08 +0545

    add some changes

--- employee.py
+++ employee.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import ttk
+
 class employeeClass: 
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1100x500+220+130")
+        self.root.geometry("1100x500+220+90")
         self.root.title("Retail Merchandise Management System | Developed by Samyam Sandip")
         self.root.config(bg="white")
         self.root.focus_force() #to highlight the window of employee whenever the employee button is pressed
@@ -19,7 +20,7 @@ class employeeClass:
         cmb_search.current(0) #Select is shown in the dropdown when nothing is selected
 
         txt_search=Entry(SearchFrame, font=("Times New Roman", 15), bg="lightyellow", width=28).place(x=170, y=7)
-        btn_search=Button(SearchFrame, text="Search", font=("Baskerville Old Face", 15), bg="#4caf50", fg="white", cursor="hand2").place(x=465, y=6, width=120, height=30)
+        btn_search=Button(SearchFrame, text="Search", font=("Times New Roman", 14, "italic", "bold"), bg="#008080", fg="white", cursor="hand2").place(x=465, y=6, width=120, height=30)
 if __name__ == "__main__":
     root = Tk()
     obj = employeeClass(root)
